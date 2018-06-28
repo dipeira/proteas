d3c72fd357018eed15c4a16297f9cf2de6dec9be
sugarvag <sugarvag@gmail.com>
2018-06-28 14:00:38 +0300

    Προσθήκη "dummy"σελίδας ανακατεύθυνσης (login_check.php) για ομαλή μετάβαση στη νέα έκδοση

---
+++ tools/login_check.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<?php
+    header('Content-type: text/html; charset=iso8859-7'); 
+?>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="refresh" content="2; URL=login.php">
+        <title>Πρωτέας</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="../css/login.css" />
+    </head>
+    <body>
+        <h3>Γίνεται ανακατεύθυνση...</h3>
+    </body>
+</html>
+
