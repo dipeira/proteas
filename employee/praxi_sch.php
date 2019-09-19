d3ebe19fb8d66fa24c274e3818d1fcf9fd124047
sugarvag <sugarvag@gmail.com>
2019-09-19 10:51:01 +0300

    Διόρθωση σφάλματος στο εργαλείο "Εκπαιδευτικοί και Σχολεία ανά Πράξη"

--- employee/praxi_sch.php
+++ employee/praxi_sch.php
@@ -5,35 +5,34 @@
 ?>	
   <html>
   <head>      
-        <LINK href="../css/style.css" rel="stylesheet" type="text/css">
-        <title>Εκπαιδευτικοί και Σχολεία ανά Πράξη</title>
-        <link href="../css/select2.min.css" rel="stylesheet" />
-       	<script type="text/javascript" src="../js/1.7.2.jquery.min.js"></script>
-        <script src="../js/select2.min.js"></script>
-        <script type="text/javascript" src="../js/jquery.tablesorter.js"></script> 
-        <script type="text/javascript">   
-            $(document).ready(function() { 
-			$("#mytbl").tablesorter({widgets: ['zebra']}); 
-                        $(".praxi_select").select2();
-		});
-            
-        </script>
-    </head>
-    <body>
+    <LINK href="../css/style.css" rel="stylesheet" type="text/css">
+    <title>Εκπαιδευτικοί και Σχολεία ανά Πράξη</title>
+    <link href="../css/select2.min.css" rel="stylesheet" />
+    <script type="text/javascript" src="../js/1.7.2.jquery.min.js"></script>
+    <script src="../js/select2.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.tablesorter.js"></script> 
+    <script type="text/javascript">   
+      $(document).ready(function() { 
+			  $("#mytbl").tablesorter({widgets: ['zebra']}); 
+        $(".praxi_select").select2();
+		  });    
+    </script>
+  </head>
+  <body>
     <?php include('../etc/menu.php'); ?>
 <?php        
 	// praxi_sch: Displays schools or Schools & ektaktoi for the selected praxi
 
-        include("../tools/class.login.php");
-        $log = new logmein();
-        if($log->logincheck($_SESSION['loggedin']) == false){
-            header("Location: ../tools/login.php");
-        }
-        $usrlvl = $_SESSION['userlevel'];
+    include("../tools/class.login.php");
+    $log = new logmein();
+    if($log->logincheck($_SESSION['loggedin']) == false){
+        header("Location: ../tools/login.php");
+    }
+    $usrlvl = $_SESSION['userlevel'];
 
-        $mysqlconnection = mysqli_connect($db_host, $db_user, $db_password, $db_name);  
-        mysqli_query($mysqlconnection, "SET NAMES 'greek'");
-        mysqli_query($mysqlconnection, "SET CHARACTER SET 'greek'");
+    $mysqlconnection = mysqli_connect($db_host, $db_user, $db_password, $db_name);  
+    mysqli_query($mysqlconnection, "SET NAMES 'greek'");
+    mysqli_query($mysqlconnection, "SET CHARACTER SET 'greek'");
         
     echo "<h2>Εκπαιδευτικοί και Σχολεία ανά Πράξη</h2>";
     echo "<table class=\"imagetable\" border='1'>";
@@ -44,10 +43,10 @@
     echo "<tr><td>Επιλογή Πράξεων:</td><td>";
     $cmb = "<select name=\"praxi[]\" class=\"praxi_select\" multiple=\"multiple\">";
     while ($row = mysqli_fetch_array($result)){
-        if (isset($_POST['praxi']) && in_array($row['id'],$_POST['praxi']))
-            $cmb .= "<option value=\"".$row['id']."\" selected>".$row['name']."</option>";
-        else
-            $cmb .= "<option value=\"".$row['id']."\">".$row['name']."</option>";
+      if (isset($_POST['praxi']) && in_array($row['id'],$_POST['praxi']))
+          $cmb .= "<option value=\"".$row['id']."\" selected>".$row['name']."</option>";
+      else
+          $cmb .= "<option value=\"".$row['id']."\">".$row['name']."</option>";
     }
     $cmb .= "</select>";
     echo $cmb;
@@ -66,50 +65,50 @@
 
 	if(isset($_POST['praxi']))
 	{
-                foreach ($_REQUEST['praxi'] as $pr)
-                    $praxeis[] = $pr;
-                $i = 0;
-                
-                $all = $_POST['type'];
-                
-                foreach ($praxeis as $pr)
-                    $praxinm[] = getNamefromTbl($mysqlconnection, praxi, $pr);
+    foreach ($_REQUEST['praxi'] as $pr)
+        $praxeis[] = $pr;
+    $i = 0;
+    
+    $all = $_POST['type'];
+    
+    foreach ($praxeis as $pr)
+        $praxinm[] = getNamefromTbl($mysqlconnection, praxi, $pr);
 
-                $mysqlconnection = mysqli_connect($db_host, $db_user, $db_password, $db_name);  
-                mysqli_query($mysqlconnection, "SET NAMES 'greek'");
-                mysqli_query($mysqlconnection, "SET CHARACTER SET 'greek'");
+    $mysqlconnection = mysqli_connect($db_host, $db_user, $db_password, $db_name);  
+    mysqli_query($mysqlconnection, "SET NAMES 'greek'");
+    mysqli_query($mysqlconnection, "SET CHARACTER SET 'greek'");
 		if ($all)
-                    $query = "select s.id as schid, e.id, e.surname, e.name, s.name as sch, p.name as praxi from ektaktoi e join yphrethsh_ekt y on e.id = y.emp_id 
-                        join school s on s.id = y.yphrethsh join praxi p on p.id = e.praxi where y.sxol_etos=$sxol_etos AND e.praxi in (" . implode(',',$_POST['praxi']) . ") ORDER BY SURNAME,NAME ASC";
-                else
-                    $query = "select distinct s.name as sch, s.id as schid from ektaktoi e join yphrethsh_ekt y on e.id = y.emp_id 
-                        join school s on s.id = y.yphrethsh where y.sxol_etos=$sxol_etos AND e.praxi in (" . implode(',',$_POST['praxi']) . ") ORDER BY S.NAME ASC";
-                //echo $query;
+      $query = "select s.id as schid, e.id, e.surname, e.name, s.name as sch, p.name as praxi from ektaktoi e join yphrethsh_ekt y on e.id = y.emp_id 
+        join school s on s.id = y.yphrethsh join praxi p on p.id = e.praxi where y.sxol_etos=$sxol_etos AND e.praxi in (" . implode(',',$_POST['praxi']) . ") ORDER BY SURNAME,NAME ASC";
+    else
+      $query = "select distinct s.name as sch, s.id as schid from ektaktoi e join yphrethsh_ekt y on e.id = y.emp_id 
+        join school s on s.id = y.yphrethsh where y.sxol_etos=$sxol_etos AND e.praxi in (" . implode(',',$_POST['praxi']) . ") ORDER BY sch ASC";
+    //echo $query;
 		$result = mysqli_query($mysqlconnection, $query);
 		
-                ob_start();
-                echo "<h2>Πράξη(-εις): ". implode(', ', $praxinm)."</h2>";
+    ob_start();
+    echo "<h2>Πράξη(-εις): ". implode(', ', $praxinm)."</h2>";
 		echo "<table id=\"mytbl\" class=\"imagetable tablesorter\" border=\"1\">";
-                if ($all)
-                    echo "<thead><tr><th>Ονοματεπώνυμο</th><th>Σχολείο</th><th>Πράξη</th></tr></thead><tbody>";
-                else
-                    echo "<thead><tr><th>Σχολείο</th></tr></thead><tbody>";
+    if ($all)
+        echo "<thead><tr><th>Ονοματεπώνυμο</th><th>Σχολείο</th><th>Πράξη</th></tr></thead><tbody>";
+    else
+        echo "<thead><tr><th>Σχολείο</th></tr></thead><tbody>";
 
-            while ($row = mysqli_fetch_array($result))	
-            {
-		$id = $row['id'];
-		$name = $row['name'];
-		$surname = $row['surname'];
-                $sch = $row['sch'];
-                $schid = $row['schid'];
-		$praxi = $row['praxi'];
+    while ($row = mysqli_fetch_array($result))	
+    {
+		  $id = $row['id'];
+		  $name = $row['name'];
+		  $surname = $row['surname'];
+      $sch = $row['sch'];
+      $schid = $row['schid'];
+		  $praxi = $row['praxi'];
 		                
-                if ($all)
-                    echo "<tr><td><a href=\"ektaktoi.php?id=$id&op=view\">$surname $name</a></td><td><a href=\"../school/school_status.php?org=$schid\">$sch</a></td><td>$praxi</td></tr>";
-                else
-                    echo "<tr><td><a href=\"../school/school_status.php?org=$schid\">$sch</a></td></tr>";
-                $i++;
-            }
+      if ($all)
+          echo "<tr><td><a href=\"ektaktoi.php?id=$id&op=view\">$surname $name</a></td><td><a href=\"../school/school_status.php?org=$schid\">$sch</a></td><td>$praxi</td></tr>";
+      else
+          echo "<tr><td><a href=\"../school/school_status.php?org=$schid\">$sch</a></td></tr>";
+      $i++;
+    }
 		echo "</tbody></table>";
     echo "<small><i>$i εγγραφές</i></small>";
     echo "<br><br>";
@@ -122,10 +121,10 @@
 			
 		echo "<form action='../tools/2excel.php' method='post'>";
 		echo "<input type='hidden' name = 'data' value='".  htmlspecialchars($page, ENT_QUOTES)."'>";
-                echo "<BUTTON TYPE='submit'><IMG SRC='../images/excel.png' ALIGN='absmiddle'>Εξαγωγή στο excel</BUTTON>";
-                echo "&nbsp;&nbsp;&nbsp;";
-                echo "<INPUT TYPE='button' class='btn-red' VALUE='Επιστροφή' onClick=\"parent.location='ektaktoi_list.php'\">";
-                echo "</form>";
+    echo "<BUTTON TYPE='submit'><IMG SRC='../images/excel.png' ALIGN='absmiddle'>Εξαγωγή στο excel</BUTTON>";
+    echo "&nbsp;&nbsp;&nbsp;";
+    echo "<INPUT TYPE='button' class='btn-red' VALUE='Επιστροφή' onClick=\"parent.location='ektaktoi_list.php'\">";
+    echo "</form>";
 	}
 ?>
 <br><br>
